7db716e935f133471be2b68712b877b7a093319d
Simon Schlegel <si_schlegel@bluewin.ch>
2023-06-01 18:12:17 +0200

    MathLibTest erstellt

---
+++ src/test/java/com/MathLibTest.java
@@ -0,0 +1,9 @@
+import static
+org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+public class MathLibTest {
+@Test
+public void testEven1() {
+assertTrue(MathLib.isEven(2));
+}
+}
