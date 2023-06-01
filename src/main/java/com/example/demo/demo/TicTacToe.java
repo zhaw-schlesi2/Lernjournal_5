21f67e1f9a494870e438bafa62313cdd96393192
Simon Schlegel <si_schlegel@bluewin.ch>
2023-06-01 20:17:43 +0200

    Changes for test 1

--- src/main/java/com/example/demo/demo/TicTacToe.java
+++ src/main/java/com/example/demo/demo/TicTacToe.java
@@ -1,8 +1,14 @@
 package com.example.demo.demo;
 
 public class TicTacToe {
-    public String play(int player, int field) {
+    String[] map = new String[9];
 
-        return "";
+
+    public String play(int player, int field) {
+        int i = 0;
+
+        map[field] = Integer.toString(player);
+        
+        return Integer.toString(player) + "," + Integer.toString(field);
     } 
 }
